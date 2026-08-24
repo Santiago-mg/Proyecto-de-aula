c802cb11c5cb5425077f6a3019ef9d8170e786ba
Santiago-mg <140726848+Santiago-mg@users.noreply.github.com>
2026-08-23 22:42:55 -0500

    Revert "Agrego frontend y backend del proyecto de aula"

--- Proyecto-de-aula-main/src/domain/repositories/IUserRepository.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from '../entities/User'
-
-export interface IUserRepository {
-  findByEmail(email: string): Promise<User | null>
-  findById(id: string): Promise<User | null>
-  create(data: { email: string; name: string; password: string }): Promise<User>
-}
